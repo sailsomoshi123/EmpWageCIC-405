08330997696450a8d7151a946d8afb3cacc44bf9
Sail Somoshi <sailsomoshi11@gmail.com>
2021-12-01 21:22:38 +0530

    wage calculation by using case statement

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -4,16 +4,9 @@ isPartTime=1
 isFullTime=2
 empRateperHr=20
 randomCheck=$((RANDOM%3))
-if [ $randomCheck -eq $isFullTime ]
-then
-	echo "employee is fulltime"
-	empHrs=8
-elif [ $randomCheck -eq $isPartTime ]
-then
-	echo "employee is parttime"
-	empHrs=4
-else
-	echo "employee is absent"
-	empHrs=0
-fi
+case $randomCheck in
+	$isFullTime) empHrs=8 ;;
+	$isPartTime) empHrs=4 ;;
+	*) empHrs=0 ;;
+esac
 salary=$(($empHrs*$empRateperHr))

7fdad08e4fc8821e1697e68abcb66a03f26b902b
Sail Somoshi <sailsomoshi11@gmail.com>
2021-12-01 21:57:02 +0530

    problem resolved

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -5,14 +5,17 @@ isFullTime=2
 totalSalary=0
 empRateperHr=20
 numWorkingDays=20
-randomCheck=$((RANDOM%3))
 for (( day=1;day<=$numWorkingDays;day++ ))
 do
+randomCheck=$((RANDOM%3))
  case $randomCheck in
 	 $isFullTime) empHrs=8 ;;
 	 $isPartTime) empHrs=4 ;;
 	 *) empHrs=0 ;;
  esac
+ salary=$(($empHrs*$empRateperHr))
+ totalSalary=$(($totalSalary+$salary))
 done
-salary=$(($empHrs*$empRateperHr))
-totalSalary=$(($salary*$numWorkingDays))
+
+echo "total salary for month is $totalSalary"
+

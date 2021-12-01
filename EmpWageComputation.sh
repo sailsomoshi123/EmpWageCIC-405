7d9e3aca84f3189a36a3e64d836762f570669229
Sail Somoshi <sailsomoshi11@gmail.com>
2021-12-01 20:45:28 +0530

    daily employee wage computation

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,10 +1,14 @@
 #! /bin/bash/ -x
 echo "welcome to employee wage computation"
-isPresent=1;
+isPresent=1
+empRateperHr=20
 randomCheck=$((RANDOM%2))
 if [ $randomCheck -eq $isPresent ]
 then
 	echo "employee is present"
+	empHrs=8
 else
 	echo "employee is absent"
+	empHrs=0
 fi
+salary=$(($empHrs*$empRateperHr))

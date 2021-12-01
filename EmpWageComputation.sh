c721ddd70411547418b8f6b19ef96ef5bc675d27
Sail Somoshi <sailsomoshi11@gmail.com>
2021-12-01 21:38:53 +0530

    wage for month

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -2,11 +2,17 @@
 echo "welcome to employee wage computation"
 isPartTime=1
 isFullTime=2
+totalSalary=0
 empRateperHr=20
+numWorkingDays=20
 randomCheck=$((RANDOM%3))
-case $randomCheck in
-	$isFullTime) empHrs=8 ;;
-	$isPartTime) empHrs=4 ;;
-	*) empHrs=0 ;;
-esac
+for (( day=1;day<=$numWorkingDays;day++ ))
+do
+ case $randomCheck in
+	 $isFullTime) empHrs=8 ;;
+	 $isPartTime) empHrs=4 ;;
+	 *) empHrs=0 ;;
+ esac
+done
 salary=$(($empHrs*$empRateperHr))
+totalSalary=$(($salary*$numWorkingDays))

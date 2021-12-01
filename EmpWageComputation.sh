c8e2844af4eefd10ccbc6643d0936aff32b8345d
Sail Somoshi <sailsomoshi11@gmail.com>
2021-12-01 20:32:51 +0530

    employee attendance

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,2 +1,10 @@
 #! /bin/bash/ -x
 echo "welcome to employee wage computation"
+isPresent=1;
+randomCheck=$((RANDOM%2))
+if [ $randomCheck -eq $isPresent ]
+then
+	echo "employee is present"
+else
+	echo "employee is absent"
+fi
